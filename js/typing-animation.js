a3a6e085d269857ae6ccc5e1a262e600c94b5ac4
Neiser <Neiser@gmail.com>
2023-04-19 09:01:06 -0500

    Actualizaciones del portafolio y mejoras en el pdf

--- js/typing-animation.js
+++ js/typing-animation.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:["", "Hello, world", "Bonjour, monde", " Hallo, Welt", "こんにちは、世界", "مرحبا بالعالم"],
+    strings:["", "Hola, Mundo", "Hello, world", "Bonjour, monde", " Hallo, Welt", "こんにちは、世界", "مرحبا بالعالم"],
     typeSpeed: 300,
     backSpeed: 60,
     loop: true
